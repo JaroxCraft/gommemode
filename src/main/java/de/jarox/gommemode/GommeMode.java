9f1c44a9ba99036d5c29f1d001092268ab772dff
jarox <georgehd03@gmail.com>
2024-11-26 19:33:11 +0100

    add keybinding for toggling gommemode

--- src/main/java/de/jarox/gommemode/GommeMode.java
+++ src/main/java/de/jarox/gommemode/GommeMode.java
@@ -5,12 +5,17 @@ import de.jarox.gommemode.command.GommemodeCommandAction;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class GommeMode implements ModInitializer, ClientModInitializer {
 
@@ -18,8 +23,8 @@ public class GommeMode implements ModInitializer, ClientModInitializer {
 
     public static Identifier GOMMEMODE_SONG = Identifier.of(MOD_ID, "song");
     public static SoundEvent GOMMEMODE_SOUND_EVENT = Registry.register(Registries.SOUND_EVENT, GOMMEMODE_SONG, SoundEvent.of(GOMMEMODE_SONG));
-
     public static GommeMode INSTANCE;
+    private KeyBinding toggleKey;
 
     public GommeMode() {
         INSTANCE = this;
@@ -31,6 +36,24 @@ public class GommeMode implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.gommemode.toggle",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                "category.gommemode.general"
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleKey.wasPressed()) {
+                if (GommemodeManager.getInstance().isActive()) {
+                    GommemodeManager.getInstance().stop();
+                } else {
+                    assert client.player != null;
+                    assert client.world != null;
+                    GommemodeManager.getInstance().start(client.player, client.world);
+                }
+            }
+        });
+
         ArgumentTypeRegistry.registerArgumentType(
                 Identifier.of(GommeMode.MOD_ID, "action"),
                 GommemodeCommandAction.ActionArgumentType.class,
