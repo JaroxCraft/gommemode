cc4d50e49fb744e9de080684e559d36bce81cd9d
jarox <jarox@jarox.de>
2024-12-24 10:06:20 +0100

    add Category back in because it needed

--- src/main/kotlin/de/jarox/gommemode/config/Config.kt
+++ src/main/kotlin/de/jarox/gommemode/config/Config.kt
@@ -1,9 +1,12 @@
 package de.jarox.gommemode.config
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder
+import me.shedaniel.clothconfig2.api.ConfigCategory
 import net.minecraft.text.Text
 
 object Config {
     val configBuilder: ConfigBuilder = ConfigBuilder.create()
         .setTitle(Text.of("Gommemode"))
+
+    var generalCategory: ConfigCategory = configBuilder.getOrCreateCategory(Text.of("general"))
 }
