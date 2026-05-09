3aeb897fe5bf720b756bfd45aadf02ac76f3816f
JaroxCraft <nico.faeh@pm.me>
2026-05-09 22:09:11 +0200

    refactor(config): extract factory method for config screen creation

--- src/main/kotlin/de/jarox/gommemode/modmenu/ModMenuApiImpl.kt
+++ src/main/kotlin/de/jarox/gommemode/modmenu/ModMenuApiImpl.kt
@@ -3,10 +3,10 @@ package de.jarox.gommemode.modmenu
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
 import de.jarox.gommemode.config.Config
-import net.minecraft.client.gui.screens.Screen
 
+/**
+ * ModMenu API implementation that provides a configuration screen factory.
+ */
 class ModMenuApiImpl : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory { parent: Screen? -> Config.configBuilder.setParentScreen(parent).build() }
-    }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> = ConfigScreenFactory { parent -> Config.buildScreen(parent) }
 }
