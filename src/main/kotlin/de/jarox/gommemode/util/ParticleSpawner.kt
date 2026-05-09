f1068f46a6315a0d1dd088a3d60710ffa3609919
JaroxCraft <nico.faeh@pm.me>
2026-05-09 22:09:23 +0200

    refactor(particle): use for loops and kotlin.math.PI in ParticleSpawner

--- src/main/kotlin/de/jarox/gommemode/util/ParticleSpawner.kt
+++ src/main/kotlin/de/jarox/gommemode/util/ParticleSpawner.kt
@@ -3,26 +3,39 @@ package de.jarox.gommemode.util
 import net.minecraft.client.multiplayer.ClientLevel
 import net.minecraft.core.particles.ParticleOptions
 import net.minecraft.world.phys.Vec3
+import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
+/**
+ * Spawns particles in a spherical pattern around the given center point.
+ *
+ * @param level The client world to spawn particles in
+ * @param center The center of the sphere
+ * @param radius The radius of the sphere
+ * @particleType The type of particle to spawn
+ * @stepSize Angular step size in radians; smaller values produce denser particle distributions
+ */
 fun spawnSphere(
-    world: ClientLevel,
+    level: ClientLevel,
     center: Vec3,
     radius: Double,
-    particle: ParticleOptions,
-    particleDensity: Double
+    particleType: ParticleOptions,
+    stepSize: Double,
 ) {
-    var phi = 0.0
-    while (phi <= Math.PI) {
-        var theta = 0.0
-        while (theta <= 2 * Math.PI) {
+    val phiSteps = (PI / stepSize).toInt()
+    val thetaSteps = (2 * PI / stepSize).toInt()
+
+    for (phiIndex in 0..phiSteps) {
+        val phi = phiIndex * stepSize
+        for (thetaIndex in 0..thetaSteps) {
+            val theta = thetaIndex * stepSize
+
             val x = center.x + radius * sin(phi) * cos(theta)
             val y = center.y + radius * sin(phi) * sin(theta)
             val z = center.z + radius * cos(phi)
-            world.addParticle(particle, x, y, z, 0.0, 0.0, 0.0)
-            theta += particleDensity
+
+            level.addParticle(particleType, x, y, z, 0.0, 0.0, 0.0)
         }
-        phi += particleDensity
     }
 }
